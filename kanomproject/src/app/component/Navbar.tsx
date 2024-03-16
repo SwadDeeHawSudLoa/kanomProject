29d3f138221409243bdc92750f417ef7cbf726ab
SwadDeeHawSudLoa <127951003+SwadDeeHawSudLoa@users.noreply.github.com>
2024-03-16 14:38:15 +0700

    first update

---
+++ kanomproject/src/app/component/Navbar.tsx
@@ -0,0 +1,32 @@
+export default function Navbar() {
+    return (
+    <>
+     <div className="w-full h-20 bg-emerald-800 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+
+                  <p>About Us</p>
+              
+              </li>
+              <li>
+            
+                  <p>Services</p>
+               
+              </li>
+              <li>
+               
+                  <p>Contacts</p>
+                
+              </li>
+            </ul>
+           
+          </div>
+        </div>
+      </div>
+    </>  
+    );
+  }
+  
